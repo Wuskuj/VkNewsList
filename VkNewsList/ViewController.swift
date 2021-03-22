4e63960a776ebb6c08030c10c8fa9e17c2b98183
Philip Bratov <p.bratau@qittiq.by>
2021-03-22 22:32:54 +0300

    Initial Commit

---
+++ VkNewsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VkNewsList
+//
+//  Created by Philip Bratov on 22.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
